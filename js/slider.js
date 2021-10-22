8d3048e39146df048acabd06bb0180c044857a85
Fernanda Gonçalves <dev.fegoncalves@gmail.com>
2021-10-22 13:18:16 -0300

    atualização e inserção dos mini banners

--- js/slider.js
+++ js/slider.js
@@ -1,1 +1,14 @@
-$('.slider').slick();
+$("#carousel").slick({
+  prevArrow: `
+    <button type="button" class="slick-prev">
+      <span><img src="../svgs/angle-left-solid.svg"></span>
+      Anterior
+    </button>
+  `,
+  nextArrow: `
+    <button type="button" class="slick-next">
+      Próximo
+      <span><img src="../svgs/angle-right-solid.svg"></span>
+    </button>
+  `
+});
